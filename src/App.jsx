6ab348968050114b352f1ba064640f53fd1fd181
StephenRudra <stephenrudra00@gmail.com>
2023-11-06 23:24:33 +0600

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Checkbox from '@mui/material/Checkbox';
+import {Draggable} from 'react-beautiful-dnd';
+import { DragDropContext } from 'react-beautiful-dnd';
+import { Droppable } from 'react-beautiful-dnd';
+//import Dragged from './Drag';
 
 function App() {
   const [count, setCount] = useState(0)
-  const handleChange= () =>{
-
-  }
+  
   return (
     <>
      <div className="navbar bg-base-100">
@@ -41,32 +43,48 @@ function App() {
   <div className='bg-purple-300 w-36 rounded h-40'>07</div>
   <div className='bg-purple-300 w-36 rounded h-40'>08</div>
   
-  <div className='bg-purple-300 w-36 rounded h-40'>10</div>
-  <div className='bg-purple-300 w-36 rounded h-40'>11</div> absolute left-80
-  <div className='bg-purple-300 w-36 rounded h-40'>12</div>
+  <div className='bg-purple-300 w-36 rounded h-40'>10</div> 
+  <div className='bg-purple-300 w-36 rounded h-40'>11</div> 
+  <div className='bg-purple-300 w-36 rounded h-40'>12</div> 
   <div className='bg-purple-300 w-36 rounded h-40'>13</div>
-  <div className='bg-purple-300 w-36 rounded h-40'>14</div>
+  <div className='bg-purple-300 w-36 rounded h-40'>14</div> <Dragged></Dragged>
   
 </div>*/}
 <section>
-<div className="grid lg:grid-cols-5 md:grid-cols-3 gap-2 ml-10 relative">
+  <DragDropContext>
+    <Droppable droppableId='images'>
+      {(provided) =>(
+        <Draggable>
+           {(provided) =>(
+<div {...provided.droppableProps} ref={provided.innerRef}  {...provided.draggableProps} {...provided.dragHandleProps} className="images grid lg:grid-cols-5 md:grid-cols-3 gap-2 ml-10 relative">
  
-<div className='bg-purple-300 w-96 rounded h-80 row-span-2 col-span-2 bg-[url()] no-repeat'><Checkbox className='' /><img src='images/image-1.webp'/></div>
+ 
+    <>
+<div className='w-96 rounded h-80 row-span-2 col-span-2'><input type="checkbox" class="checkbox absolute left-80" id="check1" /><img src='images/image-1.webp'/></div>
   
-  <div className='bg-purple-300 w-36 rounded h-40 -ml-8'><Checkbox /><img className='' src='images/image-2.webp'/></div>
-  <div className='bg-purple-300 w-36 rounded h-40 -ml-8'><Checkbox /><img src='images/image-3.webp'/></div>
-  <div className='bg-purple-300 w-36 rounded h-40 -ml-8'><Checkbox /><img src='images/image-4.webp'/></div>
-  <div className='bg-purple-300 w-36 rounded h-40 -ml-8'><Checkbox /><img src='images/image-5.webp'/></div>
-  <div className='bg-purple-300 w-36 rounded h-40 -ml-8'><Checkbox /><img src='images/image-6.webp'/></div>
-  <div className='bg-purple-300 w-36 rounded h-40 -ml-8'><Checkbox /><img src='images/image-7.webp'/></div>
+  <div className='w-36 rounded h-40 -ml-8 img'><Checkbox className='check' /><img src='images/image-2.webp'/></div>
+  <div className='w-36 rounded h-40 -ml-8'><Checkbox className='check' /><img src='images/image-3.webp'/></div>
+  <div className='w-36 rounded h-40 -ml-8'><Checkbox className='check' /><img className='draggable' src='images/image-4.webp'/></div>
+  <div className='w-36 rounded h-40 -ml-8'><Checkbox className='check' /><img src='images/image-5.webp'/></div>
+  <div className=' w-36 rounded h-40 -ml-8'><Checkbox className='check' /><img src='images/image-6.webp'/></div>
+  <div className='w-36 rounded h-40 -ml-8'><Checkbox className='check' /><img src='images/image-7.webp'/></div>
 
-  <div className='bg-purple-300 w-36 rounded h-40'><Checkbox /><img src='images/image-8.webp'/></div>
-  <div className='bg-purple-300 w-36 rounded h-40 -ml-12'><Checkbox /><img src='images/image-9.webp'/></div>
-  <div className='bg-purple-300 w-36 rounded h-40 -ml-8'><Checkbox /><img src='images/image-10.jpeg'/></div>
-  <div className='bg-purple-300 w-36 rounded h-40 -ml-8'><Checkbox /><img src='images/image-11.jpeg'/></div>
-  <div className='bg-purple-300 w-36 rounded h-40 -ml-8'>Add more</div>
+  <div className='w-36 rounded h-40'><Checkbox className='check' /><img src='images/image-8.webp'/></div>
+  <div className='w-36 rounded h-40 -ml-12'><Checkbox className='check' /><img src='images/image-9.webp'/></div>
+  <div className='w-36 rounded h-40 -ml-8'><Checkbox className='check' /><img src='images/image-10.jpeg'/></div>
+  <div className='w-36 rounded h-40 -ml-8'><Checkbox className='check' /><img src='images/image-11.jpeg'/></div>
+  <div className='w-36 rounded h-40 -ml-8'>Add more</div>
+  </>
+ 
   
 </div>
+)}
+</Draggable>
+)}
+ 
+</Droppable>
+</DragDropContext>
+
 
 </section>
     </>
